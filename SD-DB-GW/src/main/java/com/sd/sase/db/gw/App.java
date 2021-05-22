487d16157c9e49523ee02683cd46e0841c79bd64
jasonsong <jasonsong@yunify.com>
2021-05-22 14:06:36 +0800

    dev

---
+++ SD-DB-GW/src/main/java/com/sd/sase/db/gw/App.java
@@ -0,0 +1,13 @@
+package com.sd.sase.db.gw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
